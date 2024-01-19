f87685c4e03d4d5d283fdf44ddedfdea9a17da17
Danielkojo <danielkojo005@gmail.com>
2024-01-19 02:44:49 +0000

    demo done

--- screens/homeStack/TweetDetailsScreen.js
+++ screens/homeStack/TweetDetailsScreen.js
@@ -1,18 +1,27 @@
-import { useRoute } from "@react-navigation/native";
+import { useRoute,useNavigation } from "@react-navigation/native";
 import React from "react";
-import { StatusBar, StyleSheet, View } from "react-native";
+import { Button, StatusBar, StyleSheet, View } from "react-native";
+import { SafeAreaView,Text } from "react-native";
+import { useLayoutEffect } from "react";
 
 import TweetContent from "../../components/TweetContent";
 
 const TweetDetailScreen = () => {
+  const navigation = useNavigation();
+  useLayoutEffect(()=>{
+    navigation.setOptions({headerTitle: tweet.author.name})
+  })
   const {
     params: { tweet },
   } = useRoute();
   return (
-    <View testID="TweetDetailScreen" style={styles.container}>
+    <SafeAreaView style={{flex:1}}>
+
+      <View testID="TweetDetailScreen" style={styles.container}>
       <StatusBar barStyle={"light-content"} />
       <TweetContent tweet={tweet} />
     </View>
+    </SafeAreaView>
   );
 };
 
